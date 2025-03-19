d810c51d35e35ccf85d1dcbd500fae98ee0b7aad
Chris D.C <94999762+ChristianDziurza@users.noreply.github.com>
2025-03-19 08:43:25 -0300

    Exercício 2
    Tudo feito :)

---
+++ Exercicio2/exercicio2/src/Pessoa.java
@@ -0,0 +1,7 @@
+public class Pessoa {
+    String nome, telefone;
+
+    public void exibeDados(){
+        System.out.println(nome+" com o telefone: "+telefone);
+    }
+}
---
+++ Exercicio2/exercicio2/src/Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args){
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Chris";
+        pessoa1.telefone = "928074621";
+        pessoa1.exibeDados();
+    }
+}
